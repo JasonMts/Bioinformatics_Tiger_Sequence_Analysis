a7f596440cb05729b545636795ee24326d0a97e1
jasonmts <iasonasm@gmail.com>
2019-03-20 02:07:37 +0000

    Finished statistical testing for question 8, changed ORF, working on 11

--- cw1.m
+++ cw1.m
@@ -18,7 +18,6 @@ ntdensity(seq);
 
 bases = basecount(seq);
 
-
 %find orfs on both the original and the complement 
 
 orf = seqshoworfs(seq, 'MINIMUMLENGTH', 1, 'FRAMES', 'all', 'GENETICCODE', 2);
@@ -26,26 +25,50 @@ orf = seqshoworfs(seq, 'MINIMUMLENGTH', 1, 'FRAMES', 'all', 'GENETICCODE', 2);
 
 %statistician comes in:
 %a = randseq(length(seq))
-a = seq(randperm(length(seq)))
-len_a = length(a)
+orf1 = []
+orf1Sizes = []
+maxes = []
+for j=1:1000
+    a = seq(randperm(length(seq)))
+    
+    orf1Sizes = []
+    orf1 = []
 
-orf1 = seqshoworfs(a, 'MINIMUMLENGTH', 1, 'FRAMES', 'all', 'GENETICCODE', 2);
-
+    orf1 = seqshoworfs(a, 'MINIMUMLENGTH', 1, 'FRAMES', 'all', 'GENETICCODE', 2, 'nodisplay', 'true');
+    
+    %find the lengths of all orfs in random sequence. make sure not to count
+    %the starts  that dont end
+    for i=1:6
+        if(length(orf1(i).Start) > length(orf1(i).Stop))
+            orf1(i).Start = orf1(i).Start(:,1:end-1);
+        end;
+        orf1Sizes = [orf1Sizes, orf1(i).Stop - orf1(i).Start];
+    end;
+    
+    %keep track of the max every time
+    maxes = [maxes, max(orf1Sizes)];
+end;
 
 %find the lengths of all orfs in random sequence. make sure not to count
 %the starts  that dont end
-orf1Sizes =[];
-for i=1:6
-    if(length(orf1(i).Start) > length(orf1(i).Stop))
-        orf1(i).Start = orf1(i).Start(:,1:end-1);
-    end;
-    orf1Sizes = [orf1Sizes, orf1(i).Stop - orf1(i).Start];
-
-end;
 
 %max_threshold=max(orf1Sizes)
 
-max_threshold = prctile(orf1Sizes,99)
+max_lengths = maxk(maxes,10)
+
+%visualize max lengths
+figure
+buckets = 20
+hist(maxes,buckets)
+hold on;
+xlabel('Max Length (Nucleotides)'); ylabel('Number of Sequences');
+stem(1068,1,'k')
+title('Significance of Selected ORF');
+hold off;
+
+
+%find 99th percentile of maxes
+max_threshold = prctile(maxes,99)
 
 %convert to aminoacids so we can pass it to seqshoworfs
 max_nucleotides = max_threshold/3    
@@ -53,8 +76,18 @@ max_nucleotides = max_threshold/3
 %now check and find an orf 
 orf = seqshoworfs(seq, 'MINIMUMLENGTH', max_nucleotides, 'FRAMES', 'all', 'GENETICCODE', 2);
 
+
+
 possible_orf = seq(6280:7821);
 possible_orf_length = length(possible_orf)
+
+possible_orf2 = seq(13011:14516)
+
+possible_orf3 = seq(9563:10507)
+possible_orf4 = seq(4817:5884)
+
+
+
 
 
 %now let's calculate its p value
@@ -75,8 +108,12 @@ p_val = (1 - stopCodonSum)^(possible_orf_length/3);
 
 %translate to protein based on genetic code 2
 protein = nt2aa(possible_orf, 'GENETICCODE', 2);
+protein2 = nt2aa(possible_orf2, 'GENETICCODE', 2);
+protein3 = nt2aa(possible_orf3, 'GENETICCODE', 2);
+protein4 = nt2aa(possible_orf4, 'GENETICCODE', 2);
 
-protein_first_50 = protein(1:50)
+
+protein_first_50 = protein4(1:50)
 
 
 
@@ -105,6 +142,40 @@ end
 %visualize histogram
 buckets = 10
 hist(globalscores,buckets)
+hold on;
+xlabel('Score'); ylabel('Number of Sequences');
+hold off;
+
+perm = randperm(length(seq_h));
+[c, d] = swalign(possible_orf,seq_h(perm), 'ALPHABET', 'NT');
+
+showalignment(d);
+
+%now let's find global alignments between tiger and human
+
+[scores, similar_orfs] = nwalign(seq,seq_h,'ALPHABET', 'NT');
+showalignment(similar_orfs);
+
+
+%find all proteins in human genome
+for i = 1:6
+    if(length(orf_h(i).Start) > length(orf_h(i).Stop))
+            orf_h(i).Start = orf_h(i).Start(:,1:end-1);
+    end;
+    
+    
+    if(length(orf_h(i).Stop) > 0)  
+        for j = 1:length(orf_h(i).Stop)
+            humanproteins = [humanproteins, seq_h(orf_h(i).Start(j):(orf_h(i).Stop(j)-1))];
+        end;
+    end;       
+end;
+
+
+
+
+
+
 
 
 
